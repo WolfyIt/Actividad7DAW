acefbdb30de33f38f86ecbd184853a621b18852f
Joel Santoro <darkwolf999@outlook.com>
2025-03-06 19:44:08 -0600

    se modificaron varios archivos para asi en /gender del proyecto ver la tabla dinamica

--- database/factories/SuperheroFactory.php
+++ database/factories/SuperheroFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Superhero;
+use App\Models\Universo;
+use App\Models\SuperheroType;
+use App\Models\Gender;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,14 +14,21 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class SuperheroFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Superhero::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function definition(): array
     {
         return [
-            //
+            'universe_id'
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenderSeeder::class,
             UniverseSeeder::class,
+            SuperheroSeeder::class,
         ]);
 
     }
